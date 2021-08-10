fabebb4a5651804417f269fae6f922eea73f7e58
Michael Messell <messell@me.com>
2021-08-10 22:30:28 +0200

    Add log messages and refactor Run method

--- website-watcher-app/business/website-watcher.go
+++ website-watcher-app/business/website-watcher.go
@@ -3,6 +3,7 @@ package business
 import (
 	"errors"
 	"log"
+	"strings"
 )
 
 type WebsiteWatcher struct {
@@ -17,31 +18,72 @@ func NewWebsiteWatcher(repo WebsiteRepo, hc HttpClient) WebsiteWatcher {
 func (ww WebsiteWatcher) Run() error {
 
 	websites, err := ww.repo.GetAllWebsites()
+
 	if err != nil {
 		log.Fatal(err)
 		return errors.New("Was not able to fetch all websites")
 	}
 
+	return ww.evaluateWebsites(websites)
+}
+
+func (ww WebsiteWatcher) evaluateWebsites(websites []Website) error {
 	for _, website := range websites {
-		oldState, err := ww.repo.GetWebsiteState(website)
+		oldState, _ := ww.repo.GetWebsiteState(website)
 
-		if err == nil {
+		if oldState != "" {
 			log.Print("Website '" + website.Url + "' has been visited.")
-			curState, _ := ww.hc.Get()
+			curState, err := ww.updateCurrentState(website)
 
-			if curState != oldState {
-				ww.repo.PutWebsiteState(website, curState)
-
+			if err == nil && curState != oldState {
+				log.Print("State change for website '" + website.Url + "'")
 				for _, email := range website.Emails {
 					log.Print("Send email to: " + email)
 				}
 			}
 		} else {
 			log.Print("Website '" + website.Url + "' hasn't been visited.")
-			curState, _ := ww.hc.Get()
-			ww.repo.PutWebsiteState(website, curState)
+			ww.updateCurrentState(website)
 		}
 	}
 
 	return nil
+}
+
+func (ww WebsiteWatcher) updateCurrentState(website Website) (string, error) {
+	log.Print("Trying to update state for website '" + website.Url + "'")
+
+	curState, err := ww.getCurrentWebsiteState(website)
+	if err != nil {
+		log.Fatal("Couldn't get state for website: " + website.Url)
+		log.Fatal(err)
+		return *new(string), err
+	}
+
+	err = ww.repo.PutWebsiteState(website, curState)
+
+	if err != nil {
+		log.Fatal("Couldn't update state for website: " + website.Url)
+		log.Fatal(err)
+		return *new(string), err
+	}
+
+	log.Print("State updated for website '" + website.Url + "'")
+	return curState, nil
+}
+
+func (ww WebsiteWatcher) getCurrentWebsiteState(website Website) (string, error) {
+	websiteContent, err := ww.hc.Get()
+
+	if err != nil {
+		log.Fatal("Could not get state for website: " + website.Url)
+		return *new(string), err
+	}
+
+	contentArray := strings.Split(websiteContent, website.StartKey)
+	if len(contentArray) > 1 {
+		contentArray = strings.Split(contentArray[1], website.EndKey)
+	}
+
+	return contentArray[0], nil
 }
